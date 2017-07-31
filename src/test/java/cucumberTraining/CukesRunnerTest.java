934593f06f2c6a25ee1fcc1a7223a183eb04e1ed
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-07-31 10:09:33 +0300

    Cucumber Report Config

--- src/test/java/cucumberTraining/CukesRunnerTest.java
+++ src/test/java/cucumberTraining/CukesRunnerTest.java
@@ -1,12 +1,10 @@
 package cucumberTraining;
 
-
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
